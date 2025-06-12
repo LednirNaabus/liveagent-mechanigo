edbe2ecbb524b7ea6c2dfea0d3afe94e0ebcbab8
Lednir <valsubaan@yahoo.com>
2025-06-12 23:36:38 +0800

    Add FastAPI implementation - extraction of tags done adding

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import pytz
+import logging
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from core.extract_tags import extract_and_load_tags
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    """
+    Home root path - for testing purposes.
+    """
+    return {"message": "Hello World!"}
+
+@app.post("/mechanigo-liveagent/update-tags/{table_name}")
+async def update_tags(table_name: str):
+    """
+    Endpoint to update and fetch tags from the LiveAgent API on a daily basis.
+
+    This endpoint performs the following actions:
+    1. Retrieves the tags data from the LiveAgent API via the `/tags` endpoint.
+    2. Loads the fetched tags data into a specified BigQuery table.
+
+    The `table_name` parameter is used to determine which BigQuery table the tags data should be loaded into.
+
+    Returns:
+        - A JSON response containing the fetched tags data if successful.
+        - A JSON response with an error message and status if an exception occurs during the process.
+
+    Args:
+        table_name (str): The name of the BigQuery table where the tags data will be stored.
+    
+    Raises:
+        Exception: Any error encountered during the extraction or loading process will be captured and returned in the response.
+    """
+    try:
+        tags = await extract_and_load_tags(table_name)
+        return JSONResponse(tags)
+    except Exception as e:
+        return JSONResponse(content={
+            'error': str(e),
+            'status': 'error'
+        })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
---
+++ core/extract_tags.py
@@ -0,0 +1,30 @@
+import logging
+from config import config
+from utils.bq_utils import generate_schema, load_data_to_bq
+from core.liveagent import LiveAgentClient
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+async def extract_and_load_tags(table_name: str):
+    """
+    Function to extract and load tags to BigQuery.
+
+    Calls `fetch_tags()` from the `LiveAgentClient` class.
+    """
+    try:
+        async with LiveAgentClient(config.API_KEY) as client:
+            tags = await client.fetch_tags()
+            logging.info("Generating schema and loading data to BigQuery...")
+            schema = generate_schema(tags)
+            load_data_to_bq(
+                tags,
+                config.GCLOUD_PROJECT_ID,
+                config.BQ_DATASET_NAME,
+                table_name,
+                "WRITE_APPEND",
+                schema
+            )
+            return tags.to_dict(orient="records") # make object JSON serializable
+    except Exception as e:
+        logging.error(f"Exception occured during extraction and loading of tags: {e}")
+        raise
