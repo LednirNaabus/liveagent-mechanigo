2cb53e214557ca9082186112ad506572a337cb69
Lednir <valsubaan@yahoo.com>
2025-06-14 17:02:09 +0800

    Add initial and incremental /tickets extraction - Will refactor later

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 import pytz
 import logging
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 from core.extract_tags import extract_and_load_tags
-from core.extract_tickets import extract_and_load_tickets
+from core.extract_tickets import extract_and_load_tickets_incremental, extract_and_load_tickets_initial
 from core.extract_agents import extract_and_load_agents
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -65,13 +65,20 @@ async def update_tags(table_name: str):
         })
 
 @app.post("/mechanigo-liveagent/update-tickets/{table_name}")
-async def update_tickets(table_name: str):
-    """ppppp"""
+async def update_tickets(table_name: str, is_initial: bool = Query(False)):
+    """
+    """
     try:
-        now = pd.Timestamp.now(tz="UTC").astimezone(pytz.timezone("Asia/Manila"))
-        date = now - pd.Timedelta(hours=6)
-        logging.info(f"Date and time of execution: {date}")
-        tickets = await extract_and_load_tickets(date, table_name)
+        if is_initial:
+            logging.info("Running initial ticket extraction...")
+            date = pd.Timestamp("2025-01-01")
+            tickets = await extract_and_load_tickets_initial(date, table_name)
+            pass
+        else:
+            now = pd.Timestamp.now(tz="UTC").astimezone(pytz.timezone("Asia/Manila"))
+            date = now - pd.Timedelta(hours=6)
+            logging.info(f"Date and time of execution: {date}")
+            tickets = await extract_and_load_tickets_incremental(date, table_name)
         return JSONResponse(tickets)
     except Exception as e:
         logging.error(f"Exception occured while updating tickets: {e}")
--- core/extract_tickets.py
+++ core/extract_tickets.py
@@ -10,12 +10,83 @@ from config import config
 from core.liveagent import LiveAgentClient
 from utils.df_utils import fill_nan_values
 from utils.bq_utils import generate_schema, load_data_to_bq
-from utils.date_utils import set_filter, set_timezone, format_date_col
+from utils.date_utils import set_filter, set_timezone, format_date_col, FilterField
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-async def extract_and_load_tickets(date: pd.Timestamp, table_name: str):
-    filters = set_filter(date)
+async def extract_and_load_tickets_initial(date: pd.Timestamp, table_name: str):
+    """
+    This function is used for initial full load of fetching all tickets.
+    """
+    filters = set_filter(date, filter_field=FilterField.DATE_CREATED)
+    ticket_payload = {
+        "_perPage": 10,
+        "_sortDir": "DESC",
+        "_filters": filters
+    }
+    async with LiveAgentClient(config.API_KEY) as client:
+        success, ping_response = await client.ping()
+        try:
+            if success:
+                logging.info(f"Ping to '{client.BASE_URL}' successful.")
+                tickets = await client.ticket.fetch_tickets(ticket_payload, 1)
+                # Add datetime_extracted column
+                tickets["datetime_extracted"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                tickets["datetime_extracted"] = pd.to_datetime(tickets["datetime_extracted"], errors="coerce")
+                # Set timezone
+                tickets = set_timezone(
+                    tickets,
+                    "date_created",
+                    "date_changed",
+                    "last_activity",
+                    "last_activity_public",
+                    "date_due",
+                    "date_deleted",
+                    "date_resolved",
+                    target_tz=pytz.timezone("Asia/Manila")
+                )
+                # Normalize df['custom_fields'] - must be appropriate pyarrow datatype
+                tickets["custom_fields"] = tickets["custom_fields"].apply(
+                    lambda x: x[0] if isinstance(x, list) and len(x) == 1 and isinstance(x[0], dict) else None
+                )
+                logging.info("Generating schema and loading data to BigQuery...")
+                schema = generate_schema(tickets)
+                load_data_to_bq(
+                    tickets,
+                    config.GCLOUD_PROJECT_ID,
+                    config.BQ_DATASET_NAME,
+                    table_name,
+                    "WRITE_APPEND",
+                    schema
+                )
+                # Make date columns JSON serializeable
+                tickets = format_date_col(
+                    tickets,
+                    "date_created",
+                    "date_changed",
+                    "last_activity",
+                    "last_activity_public",
+                    "date_due",
+                    "date_deleted",
+                    "date_resolved",
+                    "datetime_extracted"
+                )
+                # Fill NaN values
+                tickets = fill_nan_values(tickets)
+                file_name = os.path.join('csv', f"tickets-{date}.csv")
+                tickets.to_csv(file_name, index=False)
+                return tickets.to_dict(orient="records")
+            else:
+                logging.error(f"Ping to '{client.BASE_URL}/ping' failed. Response: {ping_response}")
+        except Exception as e:
+            logging.error(f"Exception occured while extracting tickets: {e}")
+            raise
+
+async def extract_and_load_tickets_incremental(date: pd.Timestamp, table_name: str):
+    """
+    This function is for incremental or daily fetching of tickets.
+    """
+    filters = set_filter(date, filter_field=FilterField.DATE_CHANGED)
     ticket_payload = {
         "_perPage": 10,
         "_filters": filters
