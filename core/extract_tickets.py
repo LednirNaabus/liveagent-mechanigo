7725b62783da12d29817bcaa228aa92d06fe4975
Val Subaan <valsubaan@yahoo.com>
2025-06-26 15:36:54 +0800

    ADD-26: (tickets) Add logic for checking Ticket ID Duplicates before loading to BigQuery

--- core/extract_tickets.py
+++ core/extract_tickets.py
@@ -4,8 +4,8 @@ from typing import Any
 from config import config
 from core.liveagent import LiveAgentClient
 from utils.df_utils import fill_nan_values
-from utils.bq_utils import generate_schema, load_data_to_bq
 from utils.date_utils import set_filter, set_timezone, format_date_col, FilterField
+from utils.bq_utils import generate_schema, load_data_to_bq, sql_query_bq, create_table_bq
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -30,7 +30,7 @@ async def extract_and_load_tickets(date: pd.Timestamp, table_name: str, filter_f
                 logging.info(f"Extracting using the following filter: {ticket_payload['_filters']}")
                 tickets = await client.ticket.fetch_tickets(ticket_payload, 100)
                 # Add datetime_extracted column
-                tickets["datetime_extracted"] = pd.Timestamp.now().strftime("%Y-%m-%dT%H:%M:%S")
+                tickets["datetime_extracted"] = pd.Timestamp.now(tz="UTC").strftime("%Y-%m-%dT%H:%M:%S")
                 tickets["datetime_extracted"] = pd.to_datetime(tickets["datetime_extracted"], errors="coerce")
                 # Set timezone
                 tickets = set_timezone(
@@ -49,16 +49,53 @@ async def extract_and_load_tickets(date: pd.Timestamp, table_name: str, filter_f
                 tickets["custom_fields"] = tickets["custom_fields"].apply(
                     lambda x: x[0] if isinstance(x, list) and len(x) == 1 and isinstance(x[0], dict) else None
                 )
-                logging.info("Generating schema and loading data to BigQuery...")
+                logging.info("Generating schema and empty table...")
                 schema = generate_schema(tickets)
+                create_table_bq(
+                    config.GCLOUD_PROJECT_ID,
+                    config.BQ_DATASET_NAME,
+                    table_name,
+                    schema
+                )
+                # Load data to staging table
+                logging.info("Loading data to a staging table...")
+                staging_table_name = f"{table_name}_staging"
+                logging.info(f"Table staging name: {staging_table_name}")
                 load_data_to_bq(
                     tickets,
                     config.GCLOUD_PROJECT_ID,
                     config.BQ_DATASET_NAME,
-                    table_name,
-                    "WRITE_APPEND",
+                    staging_table_name,
+                    "WRITE_TRUNCATE",
                     schema
                 )
+                # Check if ticket exists
+                update_columns = [
+                    'owner_contactid', 'owner_email', 'owner_name', 'departmentid', 'agentid', 
+                    'status', 'tags', 'code', 'channel_type', 'date_created', 'date_changed', 
+                    'date_resolved', 'last_activity', 'last_activity_public', 'public_access_urlcode', 
+                    'subject', 'custom_fields', 'date_due', 'date_deleted', 'datetime_extracted'
+                ]
+                all_columns = ['id'] + update_columns
+                update_set_clauses = ',\n    '.join([f"{col} = source.{col}" for col in update_columns])
+                insert_columns = ', '.join(all_columns)
+                insert_values = ', '.join([f"source.{col}" for col in all_columns])
+                # Perform Merge operation
+                logging.info("Merging...")
+                merge_query = f"""
+                MERGE `{config.GCLOUD_PROJECT_ID}.{config.BQ_DATASET_NAME}.{table_name}` AS target
+                USING `{config.GCLOUD_PROJECT_ID}.{config.BQ_DATASET_NAME}.{staging_table_name}` AS source
+                ON target.id = source.id
+                WHEN MATCHED THEN
+                    UPDATE SET
+                        {update_set_clauses}
+                WHEN NOT MATCHED THEN
+                    INSERT ({insert_columns})
+                    VALUES ({insert_values})
+                """
+                sql_query_bq(merge_query, return_data=False)
+                drop = f"""DROP TABLE `{config.GCLOUD_PROJECT_ID}.{config.BQ_DATASET_NAME}.{staging_table_name}`"""
+                sql_query_bq(drop, return_data=False)
                 # Make date columns JSON serializeable
                 tickets = format_date_col(
                     tickets,
